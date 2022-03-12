515c4d3e3a5c254434139d5637ca063f40d4854d
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-03-12 20:17:19 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,63 @@
 <template>
   <div id="app">
-    <quill-editor v-model="texto" :options="editorOption"></quill-editor>
+    <quill-editor v-model="text" :options="options"></quill-editor>
   </div>
 </template>
 
 <script>
+import VueQuillEditor from 'vue-quill-editor';
+import Quill from 'quill';
+import { ImageDrop } from 'quill-image-drop-module';
+import ImagePaste from './quill/imagePasteClass';
+import ImageResize from './quill/ResizeImage/ImageResize';
+
+Quill.register('modules/imageDrop', ImageDrop);
+Quill.register('modules/imagePaste', ImagePaste);
+Quill.register('modules/imageResize', ImageResize);
+
+const options = {
+  placeholder: 'Inserte el texto aquí..',
+  modules: {
+    // imageDrop: true,
+    imagePaste: true,
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ header: 1 }, { header: 2 }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ script: 'sub' }, { script: 'super' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ direction: 'rtl' }],
+      [{ size: ['small', false, 'large', 'huge'] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ font: [] }],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      ['clean'],
+      ['link', 'image', 'video'],
+      ['template-partediario'],
+    ],
+    imageResize: {
+      displayStyles: {
+        backgroundColor: 'black',
+        border: 'none',
+        color: 'white',
+      },
+      modules: ['Resize', 'DisplaySize', 'Toolbar'],
+    },
+  },
+};
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    'quill-editor': VueQuillEditor.quillEditor,
+  },
   data() {
     return {
-      texto: 'h',
-      editorOption: {
-        placeholder: 'Inserte el texto aquí..',
-        modules: {
-          // imageDrop: true,
-          imagePaste: true,
-          toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            ['blockquote', 'code-block'],
-            [{ header: 1 }, { header: 2 }],
-            [{ list: 'ordered' }, { list: 'bullet' }],
-            [{ script: 'sub' }, { script: 'super' }],
-            [{ indent: '-1' }, { indent: '+1' }],
-            [{ direction: 'rtl' }],
-            [{ size: ['small', false, 'large', 'huge'] }],
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ font: [] }],
-            [{ color: [] }, { background: [] }],
-            [{ align: [] }],
-            ['clean'],
-            ['link', 'image', 'video'],
-            ['template-partediario'],
-          ],
-          imageResize: {
-            displayStyles: {
-              backgroundColor: 'black',
-              border: 'none',
-              color: 'white',
-            },
-            modules: ['Resize', 'DisplaySize', 'Toolbar'],
-          },
-        },
-      },
+      text: '',
+      options,
     };
   },
 };
 </script>
-
-<style></style>
--- src/main.js
+++ src/main.js
@@ -2,27 +2,28 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 
-import VueQuillEditor from 'vue-quill-editor';
-import Quill from 'quill';
+// import VueQuillEditor from 'vue-quill-editor';
+// import Quill from 'quill';
 
-import { ImageDrop } from 'quill-image-drop-module';
+// import { ImageDrop } from 'quill-image-drop-module';
 
-import ImagePaste from './quill/imagePasteClass';
+// import ImagePaste from './quill/imagePasteClass';
 
 import App from './App';
 
 // import ImageResize from "quill-image-resize-module";
 // alert(ImageResize)
-import Im from './quill/ResizeImage/ImageResize';
+
+// import Im from './quill/ResizeImage/ImageResize';
+
+// Quill.register('modules/imageDrop', ImageDrop);
+// Quill.register('modules/imagePaste', ImagePaste);
+// Quill.register('modules/imageResize', Im);
 
 Vue.config.productionTip = false;
 
-Quill.register('modules/imageDrop', ImageDrop);
-Quill.register('modules/imagePaste', ImagePaste);
-Quill.register('modules/imageResize', Im);
-
-Vue.use(VueQuillEditor);
-Vue.component('quill-editor', VueQuillEditor.quillEditor);
+// Vue.use(VueQuillEditor);
+// Vue.component('quill-editor', VueQuillEditor.quillEditor);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
